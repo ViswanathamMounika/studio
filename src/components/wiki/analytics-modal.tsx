0fdcc6aa7050dfa537764fec90b6604c45ca43a8
VISWANATHAM MOUNIKA <mounikav.199@gmail.com>
2025-10-22 13:48:51 +0000

    when i select a range it is going out of box , fix this

--- src/components/wiki/analytics-modal.tsx
+++ src/components/wiki/analytics-modal.tsx
@@ -134,7 +134,7 @@ export default function AnalyticsModal({ open, onOpenChange, onDefinitionClick }
                     <Button
                         variant={"outline"}
                         className={cn(
-                            "w-[280px] justify-start text-left font-normal",
+                            "justify-start text-left font-normal",
                             !dateRange && "text-muted-foreground"
                         )}
                     >
